eb00da85e252a74106c91fff6f5457aa910d041e
AleckSalazar <aleck2407@gmail.com>
2026-04-13 09:20:13 -0600

    Add files via upload

---
+++ ejemplo1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"hola 'Mundo'\n\n\n";
+    std::cout<<"Hola\"Mundo\"\n"<<std::endl;
+    cout<<"Adios\n";
+    cout"\a";
+    cout"Imprime un numero: "<<1948*5-8;
+    return 0;
+}
